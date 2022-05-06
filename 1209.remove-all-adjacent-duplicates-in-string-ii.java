f5edd9ca74821d63e23692c6bade0566fe6a016e
shravanatirtha <shravz.ceo@gmail.com>
2022-05-06 22:14:48 +0530

    refractored

--- 1209.remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209.remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 /*
  * @lc app=leetcode id=1209 lang=java
  *
@@ -14,19 +12,20 @@ class Solution {
         for (int j = 0; j < n; ++j, ++i) {
             stack[i] = stack[j];
             count[i] = i > 0 && stack[i - 1] == stack[j] ? count[i - 1] + 1 : 1;
-            if (count[i] == k) i -= k;
+            if (count[i] == k)
+                i -= k;
         }
         return new String(stack, 0, i);
         // int alphabets[] = new int[26];
         // Arrays.fill(alphabets, 0);
         // for (int i = 0; i < s.length(); i++) {
-        //     int count = (int) s.charAt(i) - 97;
-        //     alphabets[count] = count + 1;
+        // int count = (int) s.charAt(i) - 97;
+        // alphabets[count] = count + 1;
         // }
         // String result = "";
         // for (int i = 0; i < 26; i++) {
-        //     if (alphabets[i] > 0 && alphabets[i] < k)
-        //         result = result + (char) alphabets[i];
+        // if (alphabets[i] > 0 && alphabets[i] < k)
+        // result = result + (char) alphabets[i];
         // }
         // return result;
     }
