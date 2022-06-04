65984656b0e2a473b47071623ff5c2b32b32bef6
shravanatirtha <shravz.ceo@gmail.com>
2022-06-04 14:06:27 +0530

    created

---
+++ 1357.apply-discount-every-n-orders.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1357 lang=java
+ *
+ * [1357] Apply Discount Every n Orders
+ */
+
+// @lc code=start
+class Cashier {
+
+    public Cashier(int n, int discount, int[] products, int[] prices) {
+        
+    }
+    
+    public double getBill(int[] product, int[] amount) {
+        
+    }
+}
+
+/**
+ * Your Cashier object will be instantiated and called as such:
+ * Cashier obj = new Cashier(n, discount, products, prices);
+ * double param_1 = obj.getBill(product,amount);
+ */
+// @lc code=end
+
